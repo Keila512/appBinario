584f284983af6e01460dbd0b86da1d273fad1a11
Angel Zurita <af3392976@gmail.com>
2023-03-15 21:19:31 -0600

    Add project files.

---
+++ appBinario/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿Namespace My
+    Partial Class Form1
+        Inherits System.Windows.Forms.Form
+
+        'Form overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
+        End Sub
+
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
+
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()>
+        Private Sub InitializeComponent()
+            Me.btnLeer = New System.Windows.Forms.Button()
+            Me.btnEscribir = New System.Windows.Forms.Button()
+            Me.btnBinario = New System.Windows.Forms.Button()
+            Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+            Me.SuspendLayout()
+            '
+            'btnLeer
+            '
+            Me.btnLeer.Location = New System.Drawing.Point(109, 68)
+            Me.btnLeer.Name = "btnLeer"
+            Me.btnLeer.Size = New System.Drawing.Size(101, 40)
+            Me.btnLeer.TabIndex = 0
+            Me.btnLeer.Text = "LEER"
+            Me.btnLeer.UseVisualStyleBackColor = True
+            '
+            'btnEscribir
+            '
+            Me.btnEscribir.Location = New System.Drawing.Point(343, 68)
+            Me.btnEscribir.Name = "btnEscribir"
+            Me.btnEscribir.Size = New System.Drawing.Size(98, 40)
+            Me.btnEscribir.TabIndex = 1
+            Me.btnEscribir.Text = "ESCRIBIR"
+            Me.btnEscribir.UseVisualStyleBackColor = True
+            '
+            'btnBinario
+            '
+            Me.btnBinario.Location = New System.Drawing.Point(574, 68)
+            Me.btnBinario.Name = "btnBinario"
+            Me.btnBinario.Size = New System.Drawing.Size(126, 40)
+            Me.btnBinario.TabIndex = 2
+            Me.btnBinario.Text = "ESCRIBIR BINARIO"
+            Me.btnBinario.UseVisualStyleBackColor = True
+            '
+            'RichTextBox1
+            '
+            Me.RichTextBox1.Location = New System.Drawing.Point(23, 150)
+            Me.RichTextBox1.Name = "RichTextBox1"
+            Me.RichTextBox1.Size = New System.Drawing.Size(750, 279)
+            Me.RichTextBox1.TabIndex = 3
+            Me.RichTextBox1.Text = ""
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(800, 450)
+            Me.Controls.Add(Me.RichTextBox1)
+            Me.Controls.Add(Me.btnBinario)
+            Me.Controls.Add(Me.btnEscribir)
+            Me.Controls.Add(Me.btnLeer)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        Friend WithEvents btnLeer As Button
+        Friend WithEvents btnEscribir As Button
+        Friend WithEvents btnBinario As Button
+        Friend WithEvents RichTextBox1 As RichTextBox
+    End Class
+End Namespace
---
+++ appBinario/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+Imports System.Text
+
+Namespace My
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub btnLeer_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim ofd As OpenFileDialog = New OpenFileDialog()
+            ofd.Filter = "txt|*.txt"
+
+            If ofd.ShowDialog() <> DialogResult.OK Then
+                Return
+            End If
+
+            Dim sr As StreamReader = New StreamReader(ofd.FileName)
+            RichTextBox1.Text = sr.ReadToEnd()
+            sr.Close()
+        End Sub
+
+        Private Sub btnEscribr_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim sfd As SaveFileDialog = New SaveFileDialog()
+            sfd.Filter = "txt|*.txt"
+
+            If sfd.ShowDialog() <> DialogResult.OK Then
+                Return
+            End If
+
+            Dim sw As StreamWriter = New StreamWriter(sfd.FileName)
+            sw.Write(RichTextBox1.Text)
+            sw.Close()
+        End Sub
+
+        Private Sub btnBinario_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim sfd As SaveFileDialog = New SaveFileDialog()
+            sfd.Filter = "txt|*.txt"
+
+            If sfd.ShowDialog() <> DialogResult.OK Then
+                Return
+            End If
+
+            Dim sw As StreamWriter = New StreamWriter(sfd.FileName)
+            Dim binaryString As String = ToBinary(ConvertToByteArray(RichTextBox1.Text, Encoding.ASCII))
+            sw.Write(binaryString)
+            sw.Close()
+        End Sub
+
+        Public Shared Function ConvertToByteArray(ByVal str As String, ByVal encoding As Encoding) As Byte()
+            Return encoding.GetBytes(str)
+        End Function
+
+        Public Shared Function ToBinary(ByVal data As Byte()) As String
+            Return String.Join(" ", data.[Select](Function(byt) Convert.ToString(byt, 2).PadLeft(8, "0"c)))
+        End Function
+    End Class
+End Namespace
+
